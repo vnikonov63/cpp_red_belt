ccb7f60513b6646c6803fbbec1914d687261fbed
vnikonov63 <nikonov.va@yandex.ru>
2021-01-13 00:03:46 +0400

    SMTH-1

---
+++ Week 1/ComparatorGenerator/ComparatorGenerator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ComparatorGenerator
+//
+//  Created by Elbrus on 1/13/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
