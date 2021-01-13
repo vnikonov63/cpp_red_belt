4ad531bf6e1f31f56bf29fb1b0f50c78e4970234
vnikonov63 <nikonov.va@yandex.ru>
2021-01-13 22:50:27 +0400

    Really interesting excerices i do not ever need to all a varible again

---
+++ Week 1/MakrosUNIQ_ID/MakrosUNIQ_ID/main.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+using namespace std;
+#define M(t) v##t
+#define L(t) M(t)
+#define UNIQ_ID L(__LINE__)
+
+
+int main() {
+  int UNIQ_ID = 0;
+  string UNIQ_ID = "hello";
+  vector<string> UNIQ_ID = {"hello", "world"};
+  vector<int> UNIQ_ID = {1, 2, 3, 4};
+}
