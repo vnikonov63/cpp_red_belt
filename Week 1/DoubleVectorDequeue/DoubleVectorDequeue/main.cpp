b9bca112ff94c539fea118b10140b2410433fdcd
vnikonov63 <nikonov.va@yandex.ru>
2021-01-14 14:54:37 +0400

    Started a new problem a dequeue which consists of two ectors. It is not a linked list as before or a bufferized cycle with one vector so everyting is great relatively easy just going to practice the tamplate class declaration

---
+++ Week 1/DoubleVectorDequeue/DoubleVectorDequeue/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DoubleVectorDequeue
+//
+//  Created by Elbrus on 1/14/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
