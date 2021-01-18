3d75c6ed28064e044a25de7fc01397e6a85ed74c
vnikonov63 <nikonov.va@yandex.ru>
2021-01-18 23:58:48 +0400

    R1

---
+++ ReviewYellowBelt/ReviewYellowBelt/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ReviewYellowBelt
+//
+//  Created by Elbrus on 1/18/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
