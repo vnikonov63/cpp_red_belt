112b041e5a35a0d312633e17b8a69ab9b9df0959
vnikonov63 <nikonov.va@yandex.ru>
2021-01-25 16:54:19 +0400

    after adding a new data structure for the cliens as well as adjusting for one hotel at a time when it is demanded I passes a time limit test

--- Week 2/BookHotelSystem/BookHotelSystem/main.cpp
+++ Week 2/BookHotelSystem/BookHotelSystem/main.cpp
@@ -20,36 +20,44 @@ class BookingSystem {
 public:
     void Book(int64_t time, string hotelName, int clientId, int roomCount) {
         dataBase[hotelName].push_back({time, clientId, roomCount});
+        if (fastResults.count(hotelName) == 0) {
+            fastResults[hotelName] = roomCount;
+            fastResultsClients[hotelName][clientId] += 1;
+        } else {
+            fastResults[hotelName] += roomCount;
+            fastResultsClients[hotelName][clientId] += 1;
+        }
         currentTime = time;
     }
     int Clients(string hotelName) {
         adjust(hotelName);
+        /*
         set<int> uniqueClients;
-        int result = 0;
         for (const auto& el : dataBase[hotelName]) {
-            if (uniqueClients.count(el.clientId_) == 0) {
-                result++;
-                uniqueClients.insert(el.clientId_);
-            }
-        }
-        return result;
+            uniqueClients.insert(el.clientId_);
+        }*/
+        return fastResultsClients[hotelName].size();
     }
     int Rooms(string hotelName) {
         adjust(hotelName);
-        int result = 0;
-        for (const auto& el : dataBase[hotelName]) {
-            result += el.roomCount_;
-        }
-        return result;
+        return fastResults[hotelName];
     }
 private:
     map<string, deque<Booking>> dataBase;
+    map<string, int> fastResults;
+    map<string, map<int, int>> fastResultsClients;
     int64_t currentTime;
     void adjust(string hotelName) {
-        deque<Booking>& particular = dataBase[hotelName];
-        while (!particular.empty() && particular.front().time_ + 86400 <= currentTime) {
-            particular.pop_front();
-        }
+        auto& particular = dataBase[hotelName];
+        auto& call = fastResults[hotelName];
+            while (!particular.empty() && particular.front().time_ + 86400 <= currentTime) {
+                call -= particular.front().roomCount_;
+                fastResultsClients[hotelName][particular.front().clientId_]--;
+                if (fastResultsClients[hotelName][particular.front().clientId_] == 0) {
+                    fastResultsClients[hotelName].erase(particular.front().clientId_);
+                }
+                particular.pop_front();
+            }
     }
 };
 
