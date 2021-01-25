f28a1e4e161de039c1290ecf72dcf99d4f0969fd
vnikonov63 <nikonov.va@yandex.ru>
2021-01-25 13:34:38 +0400

    finfished the electronic book problem used a vector instead of map because map internally is a tree and thus logarithmic at readtime

--- Week 2/electronicBook/electronicBook/main.cpp
+++ Week 2/electronicBook/electronicBook/main.cpp
@@ -4,22 +4,31 @@
 #include <iomanip>
 #include <sstream>
 #include <vector>
+#include <iterator>
+#include <algorithm>
 
 using namespace std;
 
 class BookReader {
 public:
     
-    BookReader() : userInfoVector(100000), numberPeople(0) {}
+    BookReader() : userInfoVector(100000), numberPeople(0), pageCount(1001) {}
     void Read(int user, int page) {
         if (userInfoVector[user] != 0) {
             int temp = userInfoVector[user];
-            readCount[temp]--;
+            auto it = next(pageCount.begin(), temp + 1);
+            auto it1 = next(pageCount.begin(), page + 1);
+            transform(it, it1, it, [](int a){
+                return a - 1;
+            });
         } else {
             numberPeople++;
+            auto it = next(pageCount.begin(), page + 1);
+            transform(it, pageCount.end(), it, [](int a){
+                return a + 1;
+            });
         }
         userInfoVector[user] = page;
-        readCount[page]++;
     }
     
     double Cheer(int user) {
@@ -28,29 +37,28 @@ public:
         } else if (numberPeople == 1) {
             return 1;
         } else {
-            auto it = readCount.lower_bound(userInfoVector[user]);
-            int sum = 0;
-            if (it == readCount.begin()) {
-                return 0;
-            }
-            do {
-                --it;
-                sum += it->second;
-            } while(it != readCount.begin());
-            return (sum * 1.0) / (numberPeople - 1) ;
+            int userPageCount = userInfoVector[user];
+            return (pageCount[userPageCount] * 1.0) / (numberPeople - 1);
         }
     }
     
 private:
     vector<int> userInfoVector;
     map<int, int> readCount;
+    vector<int> pageCount;
     int numberPeople;
 };
+
+ostream& operator << (ostream& os, vector<int> v) {
+    for (const auto& el : v) {
+        os << el << " ";
+    }
+    return os;
+}
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
     BookReader myBookReader;
     
     int operationCount;
