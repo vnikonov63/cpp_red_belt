356e0c6b35aa7b06d9b39ca37144d30a31fc3d26
vnikonov63 <nikonov.va@yandex.ru>
2021-01-27 22:54:05 +0400

    solved

--- Week 3/SwapSortPointersReversedCopy/SwapSortPointersReversedCopy/main.cpp
+++ Week 3/SwapSortPointersReversedCopy/SwapSortPointersReversedCopy/main.cpp
@@ -23,8 +23,21 @@ void SortPointers(vector<T*>& pointers) {
 }
 
 template <typename T>
-void ReversedCopy(T* source, size_t count, T* destination){
-    
+void ReversedCopy(T* source, size_t count, T* destination) {
+  auto source_begin = source;
+  auto source_end = source + count;
+  auto dest_begin = destination;
+
+  for (size_t i = 0; i < count; i++) {
+    T* source_curr = source_end - i - 1;
+    T* dest_curr = dest_begin + i;
+
+    if (dest_curr < source_begin || dest_curr >= source_end) {
+      *dest_curr = *source_curr;
+    } else if (dest_curr < source_curr) {
+      Swap(source_curr, dest_curr);
+    }
+  }
 }
 
 void TestSwap() {
