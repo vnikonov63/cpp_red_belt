a05dccfd72bcbf50ef061766dbc5a2d28b83d2d3
vnikonov63 <nikonov.va@yandex.ru>
2021-02-02 01:54:58 +0300

    the implemetation of the linked list is somewhat ready - havent tested it yet on coursera - but it passes all the requirements for in embedded tests

---
+++ Week 3/SingleLinkedList/SingleLinkedList/main.cpp
@@ -0,0 +1,163 @@
+#include "test_runner.h"
+
+#include <vector>
+using namespace std;
+
+template <typename T>
+class LinkedList {
+public:
+  struct Node {
+      Node (T value_): value(value_){}
+      
+      T value;
+      Node* next = nullptr;
+  };
+
+    ~LinkedList() {
+        Node* temp = head;
+        while (temp) {
+            Node* theNextValue = temp->next;
+            delete temp;
+            temp = theNextValue;
+        }
+    }
+
+    void PushFront(const T& value) {
+        Node* newNode = new Node(value);
+        if (head == nullptr) {
+            head = newNode;
+        } else {
+            newNode->next = head;
+            head = newNode;
+        }
+    }
+    void InsertAfter(Node* node, const T& value) {
+        if (node == nullptr) {
+            PushFront(value);
+        } else {
+            Node* temp = head;
+            while (temp != node) {
+                temp = temp->next;
+            }
+            Node* newNode = new Node(value);
+            newNode->next = temp->next;
+            temp->next = newNode;
+        }
+    }
+    void RemoveAfter(Node* node) {
+        if (node == nullptr) {
+            return;
+        } else if (node->next == nullptr) {
+            return;;
+        } else {
+            Node* temp = head;
+            while (temp != node) {
+                temp = temp->next;
+            }
+            Node* toDelete = temp->next;
+            temp->next = temp->next->next;
+            delete toDelete;
+        }
+    }
+    void PopFront() {
+        if (head == nullptr) {
+            return;
+        } else if (head->next == nullptr) {
+            delete head;
+            head = nullptr;
+        } else {
+            Node* temp = head->next;
+            delete head;
+            head = temp;
+        }
+    }
+
+  Node* GetHead() { return head; }
+  const Node* GetHead() const { return head; }
+
+private:
+  Node* head = nullptr;
+};
+
+template <typename T>
+vector<T> ToVector(const LinkedList<T>& list) {
+  vector<T> result;
+  for (auto node = list.GetHead(); node; node = node->next) {
+    result.push_back(node->value);
+  }
+  return result;
+}
+
+void TestPushFront() {
+  LinkedList<int> list;
+
+  list.PushFront(1);
+  ASSERT_EQUAL(list.GetHead()->value, 1);
+  list.PushFront(2);
+  ASSERT_EQUAL(list.GetHead()->value, 2);
+  list.PushFront(3);
+  ASSERT_EQUAL(list.GetHead()->value, 3);
+
+  const vector<int> expected = {3, 2, 1};
+  ASSERT_EQUAL(ToVector(list), expected);
+}
+
+void TestInsertAfter() {
+  LinkedList<string> list;
+
+  list.PushFront("a");
+  auto head = list.GetHead();
+  ASSERT(head);
+  ASSERT_EQUAL(head->value, "a");
+
+  list.InsertAfter(head, "b");
+  const vector<string> expected1 = {"a", "b"};
+  ASSERT_EQUAL(ToVector(list), expected1);
+
+  list.InsertAfter(head, "c");
+  const vector<string> expected2 = {"a", "c", "b"};
+  ASSERT_EQUAL(ToVector(list), expected2);
+}
+
+void TestRemoveAfter() {
+  LinkedList<int> list;
+  for (int i = 1; i <= 5; ++i) {
+    list.PushFront(i);
+  }
+
+  const vector<int> expected = {5, 4, 3, 2, 1};
+  ASSERT_EQUAL(ToVector(list), expected);
+
+  auto next_to_head = list.GetHead()->next;
+  list.RemoveAfter(next_to_head); // ÑƒÐ´Ð°Ð»ÑÐµÐ¼ 3
+  list.RemoveAfter(next_to_head); // ÑƒÐ´Ð°Ð»ÑÐµÐ¼ 2
+
+  const vector<int> expected1 = {5, 4, 1};
+  ASSERT_EQUAL(ToVector(list), expected1);
+
+  while (list.GetHead()->next) {
+    list.RemoveAfter(list.GetHead());
+  }
+  ASSERT_EQUAL(list.GetHead()->value, 5);
+}
+
+void TestPopFront() {
+  LinkedList<int> list;
+
+  for (int i = 1; i <= 5; ++i) {
+    list.PushFront(i);
+  }
+  for (int i = 1; i <= 5; ++i) {
+    list.PopFront();
+  }
+  ASSERT(list.GetHead() == nullptr);
+}
+
+int main() {
+  TestRunner tr;
+  RUN_TEST(tr, TestPushFront);
+  RUN_TEST(tr, TestInsertAfter);
+  RUN_TEST(tr, TestRemoveAfter);
+  RUN_TEST(tr, TestPopFront);
+  return 0;
+}
