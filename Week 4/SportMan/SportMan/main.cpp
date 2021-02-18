a43153ac5149d0c2702ae3b9a65ab1b64089f3b7
vnikonov63 <nikonov.va@yandex.ru>
2021-02-18 21:25:50 +0400

    start the new era

---
+++ Week 4/SportMan/SportMan/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SportMan
+//
+//  Created by Elbrus on 2/18/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
