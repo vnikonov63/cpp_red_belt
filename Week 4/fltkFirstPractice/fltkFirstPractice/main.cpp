0b226f59c75973527b0f465a69881da650812125
vnikonov63 <nikonov.va@yandex.ru>
2021-02-22 23:32:57 +0400

    started new

---
+++ Week 4/fltkFirstPractice/fltkFirstPractice/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  fltkFirstPractice
+//
+//  Created by Elbrus on 2/22/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
